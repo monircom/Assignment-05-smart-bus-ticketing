ef4ccabde0db3d1fa4f03b12d347fff0c785ba1a
monircom <monircom@gmail.com>
2024-02-18 18:54:04 +0600

    Seat Section added

---
+++ scripts/script.js
@@ -0,0 +1,122 @@
+
+
+let totalSeats  = 40;
+let totalTicketSelected = 0;
+
+const allTicketBtn = document.getElementsByClassName("add-btn");
+for (const ticketBtn of allTicketBtn) {
+  ticketBtn.addEventListener("click", function (e) {
+   
+    if (totalTicketSelected <= 3) {
+        totalSeats  = totalSeats  - 1;
+      totalTicketSelected = totalTicketSelected + 1;
+      totalCost(
+        "total-cost",
+        document.getElementById("ticket-price").innerText
+      );
+      const placeName = e.target.innerText;
+      const price = document.getElementById("ticket-price").innerText;
+      const selectedContainer = document.getElementById(
+        "selected-place-container"
+      );
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      p.innerText = placeName;
+      const p2 = document.createElement("p");
+      p2.innerText = "Economy";
+      const p3 = document.createElement("p");
+      p3.innerText = price;
+      li.appendChild(p);
+      li.appendChild(p2);
+      li.appendChild(p3);
+      li.className = "flex justify-between w-full text-left mb-4";
+      selectedContainer.appendChild(li);
+      e.target.classList.add("text-white");
+      e.target.classList.add("bg-[#1DD100]");
+      e.target.classList.add("pointer-events-none");
+    } else {
+      alert("Cannot Book More Than 4 Seats");
+    }
+
+    setElementValueById("seat-count", totalTicketSelected);
+    setElementValueById("total-seats-left", totalSeats );
+    setElementValueById("grand-total", parseInt(document.getElementById("total-cost").innerText));
+   // grandTotal();
+
+    const seat = parseInt(document.getElementById("seat-count").innerText);
+    if (seat > 3) {
+      document.getElementById("discount-btn").removeAttribute("disabled");
+    } else {
+      document.getElementById("discount-btn").setAttribute("disabled", true);
+    }
+
+    document
+      .getElementById("number-phone")
+      .addEventListener("keyup", function (event) {
+        const text = event.target.value.toString().length;
+        const button = document.getElementById("modal-btn");
+        if (text > 0 && seat > 0) {
+          button.removeAttribute("disabled");
+        } else {
+          button.setAttribute("disabled", true);
+        }
+      });
+  });
+}
+
+function grandTotal() {
+  //const totalCost = document.getElementById("total-cost").innerText;
+  //const totalCost = parseInt(document.getElementById("total-cost").innerText);
+  //const convertedTotalCost = parseInt(totalCost);
+  
+
+
+
+}
+
+const btn2 = document.getElementById("discount-btn");
+btn2.addEventListener("click", function () {
+  const couponElement = getInputValueById("input-coupon");
+  if (couponElement === "NEW15") {
+    const grandTotal = document.getElementById("total-cost").innerText;
+    const convertedTotalCost = parseInt(grandTotal);
+    const discount = convertedTotalCost * 0.15;
+    const newGrandTotal = convertedTotalCost - discount;
+    setElementValueById("grand-total", newGrandTotal);
+    //document.getElementById("coupon").className = "hidden";
+    document.getElementById("coupon").classList.add("hidden"); 
+
+  } else if (couponElement === "Couple 20") {
+    
+    const grandTotal = document.getElementById("total-cost").innerText;
+    const convertedTotalCost = parseInt(grandTotal);
+    const discount = convertedTotalCost * 0.20;
+    const newGrandTotal = convertedTotalCost - discount;
+    setElementValueById("grand-total", newGrandTotal);
+    document.getElementById("coupon").classList.add("hidden");
+    //document.getElementById("coupon").className = "hidden";
+  } else {
+    alert("Invalid Coupon");
+  }
+});
+
+
+
+
+
+function setElementValueById(elementId, value) {
+    document.getElementById(elementId).innerText = value;
+  }
+  function totalCost(id, value) {
+    const totalCost = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const sum = convertedTotalCost + parseInt(value);
+    setElementValueById(id, sum);
+  }
+  
+  function getInputValueById(elementId) {
+    const input = document.getElementById(elementId);
+    const inputValue = input.value;
+    return inputValue;
+  }
+  
